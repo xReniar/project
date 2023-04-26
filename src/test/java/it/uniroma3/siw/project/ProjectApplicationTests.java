ba7055b81745322716f5ccab506ac0bb27675072
xReniar <ilaorainer@gmail.com>
2023-04-26 10:47:01 +0200

    Initial commit

---
+++ src/test/java/it/uniroma3/siw/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
