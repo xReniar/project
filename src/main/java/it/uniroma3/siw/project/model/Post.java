eff95aa08bacca2afdf94e47839626a7d8b3c6ca
xReniar <ilaorainer@gmail.com>
2023-04-27 12:17:30 +0200

    added author attribute

--- src/main/java/it/uniroma3/siw/project/model/Post.java
+++ src/main/java/it/uniroma3/siw/project/model/Post.java
@@ -1,9 +1,7 @@
 package it.uniroma3.siw.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -14,4 +12,7 @@ public class Post {
     @NotBlank
     private String title;
     private String content;
+
+    @ManyToOne
+    private User author;
 }
