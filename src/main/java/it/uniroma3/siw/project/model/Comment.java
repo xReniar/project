3b208798723440725988007f2b0a0dbdb83563c0
Gabboooo <gabriele.bianchi21@gmail.com>
2023-04-29 17:51:16 +0200

    Comment class and Post class modified

--- src/main/java/it/uniroma3/siw/project/model/Comment.java
+++ src/main/java/it/uniroma3/siw/project/model/Comment.java
@@ -1,9 +1,10 @@
 package it.uniroma3.siw.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import it.uniroma3.siw.project.model.User;
+
+import java.util.List;
+
 
 @Entity
 public class Comment {
@@ -11,6 +12,40 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String text;
+    @ManyToOne
+    private User author;
+    @OneToMany
+    private List<User> likedUsers;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<User> getLikedUsers() {
+        return likedUsers;
+    }
+
+    public void setLikedUsers(List<User> likedUsers) {
+        this.likedUsers = likedUsers;
+    }
 }
--- src/main/java/it/uniroma3/siw/project/model/Post.java
+++ src/main/java/it/uniroma3/siw/project/model/Post.java
@@ -3,16 +3,63 @@ package it.uniroma3.siw.project.model;
 import jakarta.persistence.*;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import it.uniroma3.siw.project.model.User;
+import it.uniroma3.siw.project.model.Comment;
+
+import java.util.List;
 
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotBlank
-    private String title;
-    private String content;
-
     @ManyToOne
     private User author;
+    private String text;
+    @OneToMany
+    private List<User> likedUsers;
+    @OneToMany
+    private List<Comment> comments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<User> getLikedUsers() {
+        return likedUsers;
+    }
+
+    public void setLikedUsers(List<User> likedUsers) {
+        this.likedUsers = likedUsers;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+
 }
