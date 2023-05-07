8028b4b65d5f930473036d9ad803e01d25076e95
xReniar <ilaorainer@gmail.com>
2023-05-07 23:18:17 +0200

    .

--- src/main/java/it/uniroma3/siw/project/controller/PostController.java
+++ src/main/java/it/uniroma3/siw/project/controller/PostController.java
@@ -1,18 +1,39 @@
 package it.uniroma3.siw.project.controller;
 
+import it.uniroma3.siw.project.model.Post;
+import it.uniroma3.siw.project.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class PostController {
+    @Autowired
+    PostRepository postRepository;
 
     @GetMapping(value={"/","index.html"})
     public String index(){
         return "index.html";
     }
 
-    @GetMapping("/personalAccount")
+    /*@GetMapping("/personalAccount")
     public String personalAccountForm(){
         return "personalAccount.html";
     }
+    */
+    @GetMapping("/uploadImage")
+    public String getUploadImageForm(Model model){
+        model.addAttribute("post",new Post());
+        return "uploadImageForm.html";
+    }
+
 }
