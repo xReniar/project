f840fb6251316105037b44a7d93095808bce4815
xReniar <ilaorainer@gmail.com>
2023-05-07 23:19:00 +0200

    created Photo class for the images

---
+++ src/main/java/it/uniroma3/siw/project/model/Photo.java
@@ -0,0 +1,49 @@
+package it.uniroma3.siw.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private byte[] bytes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Photo photo = (Photo) o;
+        return Objects.equals(id, photo.id) && Arrays.equals(bytes, photo.bytes);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id);
+        result = 31 * result + Arrays.hashCode(bytes);
+        return result;
+    }
+}
