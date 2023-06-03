5005d21ceda005f658201b3d4494a35a8e2335a2
xReniar <ilaorainer@gmail.com>
2023-06-03 10:52:05 +0200

    .

---
+++ src/main/java/it/uniroma3/siw/project/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package it.uniroma3.siw.project.repository;
+
+import it.uniroma3.siw.project.model.Image;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image,Long> {
+
+}
