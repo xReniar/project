dd723cde7429d9aa502535e0123dcae7e12aa216
xReniar <ilaorainer@gmail.com>
2023-06-03 10:51:26 +0200

    removed unnecessary import

--- src/main/java/it/uniroma3/siw/project/controller/PostController.java
+++ src/main/java/it/uniroma3/siw/project/controller/PostController.java
@@ -3,17 +3,10 @@ package it.uniroma3.siw.project.controller;
 import it.uniroma3.siw.project.model.Post;
 import it.uniroma3.siw.project.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Optional;
 
 @Controller
 public class PostController {
