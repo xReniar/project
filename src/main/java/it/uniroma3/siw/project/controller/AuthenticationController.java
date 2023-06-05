81cd54dd3849715f4f7b52dd1021d7c89b516192
xReniar <ilaorainer@gmail.com>
2023-06-05 22:59:55 +0200

    added mapping for "/" and reordered code

--- src/main/java/it/uniroma3/siw/project/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/project/controller/AuthenticationController.java
@@ -3,9 +3,6 @@ package it.uniroma3.siw.project.controller;
 import it.uniroma3.siw.project.model.Credentials;
 import it.uniroma3.siw.project.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,33 +20,19 @@ public class AuthenticationController {
     @Autowired
     private CredentialsService credentialsService;
 
+    @Autowired
+    GlobalController globalController;
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        return "welcomePage.html";
+    }
+
     @GetMapping(value = "/register")
     public String showRegisterForm (Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("credentials", new Credentials());
         return "registerPage.html";
-    }
-
-    @GetMapping(value = "/login")
-    public String showLoginForm (Model model) {
-        return "loginPage.html";
-    }
-
-
-    @GetMapping(value = "/")
-    public String index(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "loginPage.html";
-        }
-        else {
-            return "index.html";
-        }
-    }
-
-    @GetMapping(value = "/success")
-    public String defaultAfterLogin(Model model) {
-        return "index.html";
     }
 
     @PostMapping("/register")
@@ -59,7 +42,7 @@ public class AuthenticationController {
                                BindingResult credentialsBindingResult,
                                Model model) {
         // se user e credential hanno entrambi contenuti validi, memorizza User e the Credentials nel DB
-        if(!userBindingResult.hasErrors() && ! credentialsBindingResult.hasErrors()) {
+        if(!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
             credentials.setUser(user);
             credentialsService.saveCredentials(credentials);
             model.addAttribute("user", user);
@@ -68,4 +51,18 @@ public class AuthenticationController {
         return "registerPage.html";
     }
 
+    @GetMapping(value = "/login")
+    public String showLoginForm (Model model) {
+        return "loginPage.html";
+    }
+
+    @GetMapping(value = "/success")
+    public String defaultAfterLogin(Model model) {
+        return "index.html";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index.html";
+    }
 }
