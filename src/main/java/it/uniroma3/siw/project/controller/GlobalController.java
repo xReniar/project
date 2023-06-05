52400c2cae96ba2e7f4135e1283de8e771f8ec44
xReniar <ilaorainer@gmail.com>
2023-06-05 23:01:33 +0200

    added function that returns the user currently logged

--- src/main/java/it/uniroma3/siw/project/controller/GlobalController.java
+++ src/main/java/it/uniroma3/siw/project/controller/GlobalController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,8 +8,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import it.uniroma3.siw.project.model.Credentials;
+import it.uniroma3.siw.project.model.User;
+import it.uniroma3.siw.project.repository.CredentialsRepository;
+
 @ControllerAdvice
 public class GlobalController {
+    @Autowired
+    CredentialsRepository credentialsRepository;
+
     @ModelAttribute("userDetails")
     public UserDetails getUser() {
         UserDetails user = null;
@@ -18,5 +26,11 @@ public class GlobalController {
             user = (UserDetails) authentication.getPrincipal();
         }
         return user;
+    }
+
+    public User getAuthor(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Credentials credential = this.credentialsRepository.findByUsername(username).get();
+        return credential.getUser();
     }
 }
