9cd926f997dfe1cc20df7bc72cd7be9ed75342b2
xReniar <ilaorainer@gmail.com>
2023-06-07 22:07:10 +0200

    user can now modify profile picture

--- src/main/java/it/uniroma3/siw/project/controller/UserController.java
+++ src/main/java/it/uniroma3/siw/project/controller/UserController.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import it.uniroma3.siw.project.model.Image;
+import it.uniroma3.siw.project.model.User;
+import it.uniroma3.siw.project.repository.ImageRepository;
 import it.uniroma3.siw.project.repository.UserRepository;
 
 @Controller
@@ -12,9 +18,32 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ImageRepository imageRepository;
+
+    @Autowired
+    GlobalController globalController;
+
     @GetMapping("/user/settings")
-    public String settings(Model model){
-        return "settings.html";
+    public String settings(){
+        return "personalAccount.html";
+    }
+
+    @PostMapping("/user/updateProfilePicture")
+    public String updateProfilePicture(Model model,@RequestParam("image") MultipartFile profilePicture) throws Exception {
+        // need to have the current user and save the new and old profile pic
+        User currentUser = this.globalController.getCurrentUser();
+        Image newProfilePic = new Image(profilePicture.getBytes());
+        Image oldProfilePic = currentUser.getProfilePicture();
+
+        // setting new profile pic
+        currentUser.setProfilePicture(newProfilePic);
+        // deleting old pic and and saving new pic, updating current user too
+        this.imageRepository.deleteById(oldProfilePic.getId());
+        this.imageRepository.save(newProfilePic);
+        this.userRepository.save(currentUser);
+        
+        return "personalAccount.html";
     }
     
 }
