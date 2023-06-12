dfbaf6904f4181beb80ee4bcbab244ebabdbca6d
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-06-12 12:10:03 +0200

    removed sql

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into movie (id, email, name, surname) values(1, 'user1@user.it', 'User', 'Uno');
-insert into credentials (id, password, role, username, user_id) values (1, '$2a$10$Uvl3SLC2OQ/DudNyWC.jvuBbU7R59Ko6pBm5JWqF9xjC9GjGD1IBa', 'AUTHENTICATED', 1);
-
-insert into movie (id, email, name, surname) values(2, 'user2@user.it', 'User', 'Due');
-insert into credentials (id, password, role, username, user_id) values (2, '$2a$10$KR62HNSBFIjtFsd7so7hQODpCofmIOpPnhZ.Ibt5yxz///1UG2lC6', 'AUTHENTICATED', 2);
-
