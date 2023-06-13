eb1f0474c935fee4799b3f120ad2f07d81dbae21
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-06-13 13:57:05 +0200

    profile pages edit for follower and following

---
+++ src/main/resources/static/js/accountPage.js
@@ -0,0 +1,31 @@
+const postsButton = document.querySelector('#postsButton')
+const followersButton = document.querySelector('#followersButton')
+const followingButton = document.querySelector('#followingButton')
+
+const postContainer = document.querySelector('#postsContainer')
+const followersContainer = document.querySelector('#followersContainer')
+const followingContainer = document.querySelector('#followingContainer')
+
+window.addEventListener('DOMContentLoaded', function() {
+    followersContainer.style.display = 'none';
+    followingContainer.style.display =  'none'
+});
+
+postsButton.addEventListener('click', ()=>{
+    postsContainer.style.display='block'
+    followersContainer.style.display='none'
+    followingContainer.style.display='none'
+})
+
+followingButton.addEventListener('click', ()=>{
+    postsContainer.style.display='none'
+    followersContainer.style.display='none'
+    followingContainer.style.display='block'
+
+})
+
+followersButton.addEventListener('click', ()=>{
+    postsContainer.style.display='none'
+    followersContainer.style.display='block'
+    followingContainer.style.display='none'
+})
