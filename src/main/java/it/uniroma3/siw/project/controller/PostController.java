cdb5b461acfda7582d1f2ff6ff292d948338a774
xReniar <ilaorainer@gmail.com>
2023-06-14 23:19:03 +0200

    fixed likedUsers bug

--- src/main/java/it/uniroma3/siw/project/controller/PostController.java
+++ src/main/java/it/uniroma3/siw/project/controller/PostController.java
@@ -11,6 +11,7 @@ import it.uniroma3.siw.project.repository.UserRepository;
 import jakarta.validation.Valid;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -63,19 +64,51 @@ public class PostController {
             author.getPosts().add(post);
 
             this.userRepository.save(author);
-            this.postRepository.save(post);
+            Post savedPost = this.postRepository.save(post);
+            User currentUser = this.globalController.getCurrentUser();
+            model.addAttribute("numPosts", currentUser.getPosts().size());
+            model.addAttribute("numFollowers", currentUser.getUsersFollowers().size());
+            model.addAttribute("numFollowing", currentUser.getUsersFollowing().size());
+            model.addAttribute("posts", this.postRepository.findAll());
+            Post correctPost = this.postRepository.findById(savedPost.getId()).get();
+            correctPost.setLikedUsers(new HashSet<User>());
+            model.addAttribute("post", correctPost);
+            System.out.println("###################################################" + correctPost.getLikedUsers());
+            model.addAttribute("comment", new Comment());
+            model.addAttribute("comments", correctPost.getComments());
+            return "post.html";
         }
-        User currentUser = this.globalController.getCurrentUser();
-        model.addAttribute("numPosts", currentUser.getPosts().size());
-        model.addAttribute("numFollowers", currentUser.getUsersFollowers().size());
-        model.addAttribute("numFollowing", currentUser.getUsersFollowing().size());
-        model.addAttribute("posts", this.postRepository.findAll());
-        return "index.html";
+        return "formNewPost.html";
     }
 
     @GetMapping("/user/post/{postId}")
     public String getPost(Model model,@PathVariable("postId") Long id){
         Post post = this.postRepository.findById(id).get();
+        model.addAttribute("post", post);
+        System.out.println("###################################################" + post.getLikedUsers());
+        if(!this.postRepository.hasCommentWithAuthor(post, this.globalController.getCurrentUser()))
+            model.addAttribute("comment", new Comment());
+        model.addAttribute("comments", post.getComments());
+        return "post.html";
+    }
+
+    @GetMapping("/user/post/like/{postId}")
+    public String likePost(Model model,@PathVariable("postId") Long id){
+        Post post = this.postRepository.findById(id).get();
+        post.getLikedUsers().add(this.globalController.getCurrentUser());
+        this.postRepository.save(post);
+        model.addAttribute("post", post);
+        if(!this.postRepository.hasCommentWithAuthor(post, this.globalController.getCurrentUser()))
+            model.addAttribute("comment", new Comment());
+        model.addAttribute("comments", post.getComments());
+        return "post.html";
+    }
+
+    @GetMapping("/user/post/unlike/{postId}")
+    public String unlikePost(Model model,@PathVariable("postId") Long id){
+        Post post = this.postRepository.findById(id).get();
+        post.getLikedUsers().remove(this.globalController.getCurrentUser());
+        this.postRepository.save(post);
         model.addAttribute("post", post);
         if(!this.postRepository.hasCommentWithAuthor(post, this.globalController.getCurrentUser()))
             model.addAttribute("comment", new Comment());
