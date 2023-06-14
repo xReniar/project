bddfe9508f7bce288e07d6e62e3fb1c6c0ac1bcf
xReniar <ilaorainer@gmail.com>
2023-06-14 13:38:19 +0200

    generation type to identity

--- src/main/java/it/uniroma3/siw/project/model/User.java
+++ src/main/java/it/uniroma3/siw/project/model/User.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String name;
