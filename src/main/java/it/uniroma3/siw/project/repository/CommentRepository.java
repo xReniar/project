c4bdf491ba7bf4ed3d84b4135b1927ca4048acff
xReniar <ilaorainer@gmail.com>
2023-06-15 21:58:04 +0200

    .

--- src/main/java/it/uniroma3/siw/project/repository/CommentRepository.java
+++ src/main/java/it/uniroma3/siw/project/repository/CommentRepository.java
@@ -3,7 +3,6 @@ package it.uniroma3.siw.project.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.project.model.Comment;
+public interface CommentRepository extends CrudRepository<Comment,Long> {
 
-public interface CommentRepository extends CrudRepository<Comment,Long>{
-    
 }
