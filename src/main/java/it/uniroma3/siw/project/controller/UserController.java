e4a8b849c612d33bccffef20681a10d1ba73abc5
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-06-15 17:58:40 +0200

    userSearch base?

--- src/main/java/it/uniroma3/siw/project/controller/UserController.java
+++ src/main/java/it/uniroma3/siw/project/controller/UserController.java
@@ -105,4 +105,12 @@ public class UserController {
         model.addAttribute("following",userToUnfollow.getUsersFollowing());
         return "userAccount.html";
     }    
+
+    @GetMapping("/user/search/{substring}")
+    public String searchUsers(Model model, @PathVariable("substring") String substring){
+
+        model.addAttribute("users", this.userRepository.findByUsernameContainingSubstring(substring));
+
+        return "userSearchResult.html";
+    }
 }
--- src/main/java/it/uniroma3/siw/project/repository/UserRepository.java
+++ src/main/java/it/uniroma3/siw/project/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package it.uniroma3.siw.project.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.project.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     public boolean existsByEmail(String email);
+
+    /* CUSTOM QUERY - trova tutti gli user i cui username contengono la sottostringa */
+    @Query("SELECT u from users u where u.username LIKE '%:substring%'")
+    List<User> findByUsernameContainingSubstring(@Param("substring") String substring);
+
 }
