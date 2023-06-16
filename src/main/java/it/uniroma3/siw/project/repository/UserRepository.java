baf8180481a17cb75a92be701931e23825b50bad
xReniar <ilaorainer@gmail.com>
2023-06-16 14:13:19 +0200

    implemented mapping for searching user

--- src/main/java/it/uniroma3/siw/project/repository/UserRepository.java
+++ src/main/java/it/uniroma3/siw/project/repository/UserRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.project.model.User;
 
@@ -12,7 +11,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     public boolean existsByEmail(String email);
 
     /* CUSTOM QUERY - trova tutti gli user i cui username contengono la sottostringa */
-    @Query(value = "SELECT * FROM users WHERE username LIKE %:substring%", nativeQuery = true)
-    List<User> findByUsernameContainingSubstring(@Param("substring") String substring);
+    @Query(value = "SELECT * FROM users WHERE UPPER(username) LIKE UPPER(CONCAT('%',:substring,'%'))", nativeQuery = true)
+    List<User> findByUsernameContainingSubstring(String substring);
 
 }
